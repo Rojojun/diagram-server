06d8faafafb1974b5634f84d6dbcfabf772397e6
rojojun <zoloman316@gmail.com>
2025-12-24 11:51:39 +0900

    feat: Add User domain

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,13 +1,39 @@
 package domain
 
 type User struct {
-	ID       string
-	Email    string
-	Password string
-	Name     string
-	Role     string
+	id       string
+	email    string
+	password string
+	name     string
 }
 
-func (u *User) Validate() error {
-	return nil
+func NewUser(email, password, name string) User {
+	if len(password) < 8 {
+	}
+
+	return User{email: email, password: password, name: name}
+}
+
+func (u *User) ID() string {
+	return u.id
+}
+
+func (u *User) Email() string {
+	return u.email
+}
+
+func (u *User) Password() string {
+	return u.password
+}
+
+func (u *User) Name() string {
+	return u.name
+}
+
+func (u *User) SetID(id string) {
+	u.id = id
+}
+
+func (u *User) SetEmail(encrypt string) {
+	u.password = encrypt
 }
